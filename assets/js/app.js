0e6114f38b08ce7226fdc06b94538cd8b4257ef9
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-04-25 17:35:44 +0800

    improve btn ui

--- assets/js/app.js
+++ assets/js/app.js
@@ -97,13 +97,12 @@ angular
     var template = `
       <div class="clearfix workplace">
         <header>
-          {{vm.editting}}
           <h1>Mocer<span> - Setup mock server easy</span></h1>
-          <button ng-if="!vm.editting" class="btn btn-success btn-edit" ng-click="vm.edit()">Edit</button>
-          <button ng-if="vm.editting" class="btn btn-success btn-edit" ng-click="vm.save()">Save</button>
+          <button ng-if="!vm.editting" class="btn btn-default btn-edit" ng-click="vm.edit()">Edit</button>
+          <button ng-if="vm.editting" class="btn btn-default btn-edit" ng-click="vm.save()">Save</button>
         </header>
         <section>
-          <div ng-show="vm.editting" class="code-editor pull-left">
+          <div ng-if="vm.editting" class="code-editor pull-left">
             <textarea ui-codemirror="cmOption" ng-model="vm.codeContent"></textarea>
           </div>
           <div class="code-preview pull-right" id="code" ng-class="{editting: vm.editting}"></div>
